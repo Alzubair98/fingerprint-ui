ee89db8a53128121e2f366ca9f68be28fad6362f
Alzubair98 <zubairsk53@gmail.com>
2025-09-20 11:31:37 +0300

    activate dark mode css

--- src/composable/useTheme.ts
+++ src/composable/useTheme.ts
@@ -1,7 +1,13 @@
-import { useColorMode, useToggle, useDark } from '@vueuse/core'
+import { useToggle, useDark } from '@vueuse/core'
 
 export function useTheme() {
-  const isDark = useDark()
+  const isDark = useDark({
+    selector: 'html',
+    attribute: 'class',
+    valueDark: 'dark',
+    valueLight: '',
+    storageKey: 'vueuse-color-scheme',
+  })
 
   const toggle = useToggle(isDark)
 
